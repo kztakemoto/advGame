bb67b158d6e789405acbd1407653c44894054f8a
kztakemoto <33507065+kztakemoto@users.noreply.github.com>
2024-06-28 19:02:02 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,6 @@ eps_values=(0.1 0.2 0.0 -0.1 -0.2)
 
 for eps in "${eps_values[@]}"; do
   for b in "${b_values[@]}"; do
-    echo ER $b $kave $eps
     python run_Game.py --payoff_b "$b" --eps "$eps" --network ER
   done
 done
